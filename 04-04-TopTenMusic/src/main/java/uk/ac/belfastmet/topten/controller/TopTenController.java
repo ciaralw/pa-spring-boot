e1c406b6b0ff69cc0a21417d463ed26142fd316f
ciaralw <55385406+ciaralw@users.noreply.github.com>
2019-09-20 11:59:36 +0100

    added error page and logs

--- 04-04-TopTenMusic/src/main/java/uk/ac/belfastmet/topten/controller/TopTenController.java
+++ 04-04-TopTenMusic/src/main/java/uk/ac/belfastmet/topten/controller/TopTenController.java
@@ -1,5 +1,6 @@
 package uk.ac.belfastmet.topten.controller;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +9,20 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import uk.ac.belfastmet.topten.service.TopTenService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller
 @RequestMapping
 
 public class TopTenController {
 	
+	Logger logger = LoggerFactory.getLogger(TopTenController.class);
+		
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	//@GetMapping("")
 	public String homePage() {
+		logger.info("End of home page method");
 		return "index";
 	}
 	
@@ -27,6 +34,7 @@ public class TopTenController {
 		model.addAttribute("pageTitle", "Top Ten Singles");
 		model.addAttribute("singles", topTenService.getTopTenSingles());
 		
+		logger.info("End of top singles page method");
 		return "topsingles";
 	}
 	
@@ -38,7 +46,15 @@ public class TopTenController {
 		model.addAttribute("pageTitle", "Top Ten Albums");
 		model.addAttribute("albums", topTenService.getTopTenAlbums());
 		
+		logger.info("End of top albums page method");
 		return "topalbums";
 	}
 
+//	@RequestMapping (value = "/error", method = RequestMethod.GET)
+//	public String errorPage() {
+//		logger.info("End of error page method");
+//		return "error";	
+//	}
+	
+	
 }
